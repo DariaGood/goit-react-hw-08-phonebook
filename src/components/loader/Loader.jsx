592481a4a2005f66afe54af9568fbbf1d2657d29
DariaGood <102043020+DariaGood@users.noreply.github.com>
2023-06-26 23:45:02 +0300

    fix 3

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,16 +1,18 @@
 import { Box } from 'components/styles/Element.styled';
-import { ColorRing } from 'react-loader-spinner';
+import { RevolvingDot } from 'react-loader-spinner';
 
 export const Loader = () => (
   <Box>
-    <ColorRing
-      visible={true}
-      height="160"
-      width="160"
-      ariaLabel="blocks-loading"
+    <RevolvingDot
+      height="100"
+      width="100"
+      radius="25"
+      color="#4fa94d"
+      secondaryColor=""
+      ariaLabel="revolving-dot-loading"
       wrapperStyle={{}}
-      wrapperClass="blocks-wrapper"
-      colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      wrapperClass=""
+      visible={true}
     />
   </Box>
 );
